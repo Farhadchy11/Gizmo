d21946ac04a5fd34850a1d660b3de919bef5a76a
unknown <farhadchy500@gmail.com>
2025-09-11 10:22:39 +0600

    updated

--- client/src/app/Store.jsx
+++ client/src/app/Store.jsx
@@ -1,10 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-// import   '../features/cartSlice/cartSlice.jsx';
-// import cs from '../components/Product/cartSlice.jsx'; 
-import cartS from '../components/Cart/cartSlice.jsx';
+import { configureStore } from "@reduxjs/toolkit";
+import cartS from "../components/Cart/cartSlice.jsx";
 
 export default configureStore({
   reducer: {
     cart: cartS,
-  }
-})
+  },
+});
