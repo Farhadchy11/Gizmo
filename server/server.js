27c062d5f075cf951dd4c1d94bf9a1823732a99d
unknown <farhadchy500@gmail.com>
2025-09-13 12:26:47 +0600

    updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import cors from "cors";
 const app = express();
-// import cors from "cors";
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 const PORT = 5050;
+
+const allowedOrigins = [
+  "https://ecommerceclient-299f6w3yb-farhads-projects-63b63f68.vercel.app",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 app.post("/jwt", async (req, res) => {
   const user = req.body;
