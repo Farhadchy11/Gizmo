911d9689940ce7ae7fa596bf20e5e151194e02ea
unknown <farhadchy500@gmail.com>
2025-09-13 12:32:46 +0600

    updated

--- client/src/Providers/AuthProvider.jsx
+++ client/src/Providers/AuthProvider.jsx
@@ -8,7 +8,7 @@ import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   //signInWithPopup,
-  signOut
+  signOut,
 } from "firebase/auth";
 import { app } from "../Firebase/firebase.config.js";
 //import App from "./App.jsx";
@@ -31,7 +31,7 @@ const AuthProvider = ({ children }) => {
         console.log(currentUser.email);
         const userInfo = { email: currentUser.email };
         axios
-          .post("https://ecommerceserver-8rdhblj27-farhads-projects-63b63f68.vercel.app/jwt", userInfo)
+          .post("https://ecommerceserver-mocha.vercel.app/jwt", userInfo)
           .then((res) => {
             console.log("JWT Response:", res.data.token);
 
