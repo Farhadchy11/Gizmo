b7e04291c8866f8d5871d2f13e36ca9972b59fc8
unknown <farhadchy500@gmail.com>
2025-11-01 18:32:45 +0600

    Initial project commit

--- client/src/features/Dashboard/AllProducts/AllProducts.jsx
+++ client/src/features/Dashboard/AllProducts/AllProducts.jsx
@@ -1,4 +1,4 @@
-import useAxiosSecure from "../../../hooks/useAxiosSecure.jsx";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure.jsx";
 import { FaDeleteLeft } from "react-icons/fa6";
 import { useGetProducts } from "../../../Hooks/useGetProducts.jsx";
 
