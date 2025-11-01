fd3b4363f6b8f294fd92fc484f2536ac4db9bcf3
unknown <farhadchy500@gmail.com>
2025-11-01 17:22:32 +0600

    Initial project commit

--- client/src/app/products/Products.jsx
+++ client/src/app/products/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useGetProducts } from "../../Hooks/useGetProducts.jsx";
-import ProductItems from "../../features/products/components/productItems.jsx";
+import ProductItems from "../../features/products/components/productitems.jsx";
 
 const Products = () => {
   const { data: products, isLoading, isError, error } = useGetProducts();
